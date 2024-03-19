645baa4accb1e66bf4a635b74a5dc68c76bac028
Bill <bill.kimtai@student.moringaschool.com>
2024-03-19 10:52:37 +0300

    control flow lab

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+      case 'generous':
+          response = 'Thank you so much.';
+          break;
+      case 'not as generous':
+          response = 'Thank you.';
+          break;
+      default:
+          response = 'Bye.';
+          break;
+  }
+  return response;
+}
+function scuberGreetingForFeet(distance) {
+  let response;
+  if (distance <= 400) {
+      response = 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+      response = 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+      response = 'I will gladly take your thirty bucks.';
+  } else {
+      response = 'No can do.';
+  }
+  return response;
+}
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+
